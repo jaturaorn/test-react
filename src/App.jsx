66299f8e8a883ba626f75a59207b2ce5f437da4b
jaturaporn <83279262+jaturaorn@users.noreply.github.com>
2023-04-10 22:26:56 +0700

    create folder component,add file header

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import Header from './Component/Header'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+      <Header />
     </div>
   )
 }
