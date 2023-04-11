b57cc5742c1173121a0a64eac984373d6ddacb08
jaturaporn <83279262+jaturaorn@users.noreply.github.com>
2023-04-11 22:40:17 +0700

    add divform-boxs,add cssform-boxs

--- src/App.jsx
+++ src/App.jsx
@@ -9,8 +9,10 @@ function App() {
       <section className='BoxLG'>
           <h1>ToDos APP</h1>
         <form className='form-box'>
+          <div className='form-boxs'>
             <input type='text' className='text-input' placeholder="ToDos" />
             <button type='submit' className='submit-btn'>Add Dos</button>
+          </div>
         </form>
       </section>
     </div>
