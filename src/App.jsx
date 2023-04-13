a9deda0dfa88bbeaeaf680129bb2bd72a883a1fe
jaturaporn <83279262+jaturaorn@users.noreply.github.com>
2023-04-13 21:00:04 +0700

    create state,delete compo Form,Head

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [Name, setName] = useState(0)
 
   return (
     <div>
@@ -10,7 +10,11 @@ function App() {
           <h1>ToDos APP</h1>
         <form className='form-box'>
           <div className='form-boxs'>
-            <input type='text' className='text-input' placeholder="ToDos" />
+            <input type='text' className='text-input' placeholder="ToDos" 
+              onChange={(e) => setName(e.target.value)} 
+              value={Name}
+            />
+
             <button type='submit' className='submit-btn'>Add Dos</button>
           </div>
         </form>
