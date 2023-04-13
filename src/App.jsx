c5f5dd550b0db47ebdbc2c0d276d97125d6bdd47
jaturaporn <83279262+jaturaorn@users.noreply.github.com>
2023-04-13 21:21:16 +0700

    create function sumitData add commented

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,23 @@ import './App.css'
 
 function App() {
   const [Name, setName] = useState('')
-
+  // ดักจับ Evnet ใน form
+  const submitData = (e) => {
+    e.preventDefault()
+    
+  }
   return (
     <div>
       <section className='BoxLG'>
           <h1>ToDos APP</h1>
-        <form className='form-box'>
+          {/* เมื่อมีการกด sumbitdata จะทำหน้าที่สร้างรายการ */}
+        <form className='form-box' onSubmit={submitData}>
           <div className='form-boxs'>
             <input type='text' className='text-input' placeholder="ToDos" 
               onChange={(e) => setName(e.target.value)} 
               value={Name}
+              //เมื่อกรอกข้อมูล (onChange) แล้วไปเก็บที่ตัวแปร Name
             />
-
             <button type='submit' className='submit-btn'>Add Dos</button>
           </div>
         </form>
