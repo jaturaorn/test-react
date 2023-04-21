b82059987075b6739647643c5cfafbf330283b49
jaturaporn <83279262+jaturaorn@users.noreply.github.com>
2023-04-21 21:41:35 +0700

    ceate cardweather

---
+++ src/Component/WeatherCard.jsx
@@ -0,0 +1,16 @@
+
+
+const WeatherCard = () => {
+  return (
+    <>
+        <div className="card">
+            <div className="weather">
+                <h1>28</h1>
+                <small>max:37 , min : 20</small>
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default WeatherCard
