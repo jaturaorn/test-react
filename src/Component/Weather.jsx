311e34f06422e90cb0a1eaca83fba27a8894f30c
jaturaporn <83279262+jaturaorn@users.noreply.github.com>
2023-04-22 12:04:08 +0700

    done but have little issue with City.sys.country

--- src/Component/Weather.jsx
+++ src/Component/Weather.jsx
@@ -1,36 +1,44 @@
-import { useEffect, useState } from 'react';
-import '../Appweather.css'
+import { useEffect, useState } from "react";
+import "../Appweather.css";
 
 function Weather() {
-    const name = 'Bangkok';
-    const apiKey = '7475b77179c89113e27e51237ab4e42b';
-    const [City,setCity] = useState({});
+  const name = "krabi";
+  const apiKey = "7475b77179c89113e27e51237ab4e42b";
+  const [City, setCity] = useState({});
 
+  useEffect(() => {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${name}&appid=${apiKey}`;
+    fetch(url)
+    .then((res) => res.json())
+    .then((data) =>{
+        setCity(data)
+    })
+  }, []);
 
-    useEffect(()=>{
-
-    },[])
+  const convertTemp = (k) => {
+    return (k-273).toFixed()
+  }
   return (
-    <div className='App'>
-        <section>
-            <div className="location">
-                <h1 className="city">Bangkok</h1>
-                <p className="state">TH</p>
-            </div>
-                <div className="card">
-                    <div className="weather">
-                        <h1>28</h1>
-                        <small>max:37 , min : 20</small>
-                    </div>
-                <div className="info">
-                    <div className="status">อากาศดี</div>
-                    <div className="humidity">100</div>
-                    <div className="wind">4.0</div>
-                </div>
-            </div>
-        </section>
+    <div className="App">
+      <section>
+        <div className="location">
+          <h1 className="city">{City.name}</h1>
+          <p className="state">{City.sys.country}</p>
+        </div>
+        <div className="card">
+          <div className="weather">
+            <h1>{convertTemp(City.main.temp)} &deg; C</h1>
+            <small>max:{convertTemp(City.main.temp_max)}&deg; C , min :{convertTemp(City.main.temp_min)}&deg; C</small>
+          </div>
+          <div className="info">
+            <div className="status">{City.weather[0].main}</div>
+            <div className="humidity">ความชื้น = {City.main.humidity}</div>
+            <div className="wind">ความเร็วลม = {City.wind.speed}</div>
+          </div>
+        </div>
+      </section>
     </div>
-  )
+  );
 }
 
-export default Weather
+export default Weather;
