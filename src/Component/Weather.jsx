6e8778da5363ce6f123d178ee7ac941059476214
jaturaporn <83279262+jaturaorn@users.noreply.github.com>
2023-04-22 18:17:45 +0700

    fix issue props in {City.sys.country}

--- src/Component/Weather.jsx
+++ src/Component/Weather.jsx
@@ -2,42 +2,44 @@ import { useEffect, useState } from "react";
 import "../Appweather.css";
 
 function Weather() {
-  const name = "krabi";
+  const name = "Bangkok";
   const apiKey = "7475b77179c89113e27e51237ab4e42b";
   const [City, setCity] = useState({});
+  const [isLoading,setisLoading] = useState(false);
 
   useEffect(() => {
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${name}&appid=${apiKey}`;
     fetch(url)
     .then((res) => res.json())
     .then((data) =>{
-        setCity(data)
+      setCity(data)
+      setisLoading(true)
     })
-  }, []);
+  }, [name]);
 
   const convertTemp = (k) => {
     return (k-273).toFixed()
   }
   return (
-    <div className="App">
-      <section>
-        <div className="location">
-          <h1 className="city">{City.name}</h1>
-          <p className="state">{City.sys.country}</p>
+    (isLoading && <div className="App">
+    <section>
+      <div className="location">
+        <h1 className="city">{City.name}</h1>
+        <p className="state">{City.sys.country}</p>
+      </div>
+      <div className="card">
+        <div className="weather">
+          <h1>{convertTemp(City.main.temp)} &deg; C</h1>
+          <small>max:{convertTemp(City.main.temp_max)}&deg; C , min :{convertTemp(City.main.temp_min)}&deg; C</small>
         </div>
-        <div className="card">
-          <div className="weather">
-            <h1>{convertTemp(City.main.temp)} &deg; C</h1>
-            <small>max:{convertTemp(City.main.temp_max)}&deg; C , min :{convertTemp(City.main.temp_min)}&deg; C</small>
-          </div>
-          <div className="info">
-            <div className="status">{City.weather[0].main}</div>
-            <div className="humidity">ความชื้น = {City.main.humidity}</div>
-            <div className="wind">ความเร็วลม = {City.wind.speed}</div>
-          </div>
+        <div className="info">
+          <div className="status">{City.weather[0].main}</div>
+          <div className="humidity">ความชื้น = {City.main.humidity}</div>
+          <div className="wind">ความเร็วลม = {City.wind.speed}</div>
         </div>
-      </section>
-    </div>
+      </div>
+    </section>
+  </div>)
   );
 }
 
