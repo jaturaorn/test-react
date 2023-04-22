3d3678dbdfc9d04cfcae48dde01e4c1c655d4f70
jaturaporn <83279262+jaturaorn@users.noreply.github.com>
2023-04-22 20:14:18 +0700

    almost done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import './Appweather.css'
+import './Slider.css';
 import Weather from './Component/Weather';
+import Imageslider from './Component/imageslider';
 
 function App() {
   return (
     <div>
-      <Weather />
+      <Imageslider />
     </div>
   )
 }
--- src/Component/imageslider.jsx
+++ src/Component/imageslider.jsx
@@ -1,13 +1,36 @@
 import Imagedata from "./imgesdata"
+import '../Slider.css'
+import { AiOutlineArrowLeft,AiOutlineArrowRight } from "react-icons/ai";
+
 
 const Imageslider = () => {
+    const [Current,setCurrent] = useState(0);
+    const length = Imagedata.length
+
+    const prevSlide =()=>{
+        if(Current === 0){
+            setCurrent(length-1)
+        }else{
+            setCurrent(Current-1)
+        }
+    }
+    const nextSlide =()=>{
+        if(Current === length-1){
+            setCurrent(0)
+        }else{
+            setCurrent(Current+1)
+        }
+    }
+
     return (
     <section className="slider">
+        <AiOutlineArrowLeft className="leftArrow" onClick={prevSlide}/>
+        <AiOutlineArrowRight className="rightArrow" onClick={nextSlide}/>
         {Imagedata.map((data, index) =>{
             return (
-                <div className="slide">
+                <div className={index === Current ? "slide active" : "slide"} key={index}>
                     <div>
-                        <img src={data.image} alt={data.title}/>
+                        <img src={data.image} alt={data.title} className="image"/>
                     </div>
                 </div>
             )
